0d18999b71854b4ff944310c0d1c1da657fe90d8
seoon <hvarnony@inu.ac.kr>
2021-08-19 03:05:10 +0900

    seoon Popup update

---
+++ app/src/main/java/com/example/a21_hg095_java/InformationScrollViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.a21_hg095_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InformationScrollViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information_scroll_view);
+
+        }
+    }
