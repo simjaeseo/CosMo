aeacd221c08c4982fcb34065367e5e8bbaf52954
simjaeseo <delpho@naver.com>
2021-08-30 01:04:23 +0900

    [MODIFIED] bluethooth test2

--- raspherry_bluetooth_test.py
+++ raspherry_bluetooth_test.py
@@ -3,16 +3,40 @@ from socket import *
 import threading
 import time
 
-def send(server_socket):
+import RPi.GPIO as GPIO
+import serial
+from time import sleep
+
+port1 = "/dev/ttyUSB1"
+serialToArduino = serial.Serial(port1,9600)
+
+def send(client_socket):
  while True:
    sendData =input('')
    server_socket.send(sendData.encode('utf-8'))
 
-def receive(server_socket):
+def receive(client_socket):
  while True:
-   recvData = server_socket.recv(1024)
+   recvData = client_socket.recv(1024)
    print(recvData.decode('utf-8'))
-
+   
+   
+   message = "500"
+   endMark = 'E'
+   
+   if recvData==None:
+      message = "500"
+      print("if1")
+   else:
+      message = "600"
+      print(message)
+   
+   serialToArduino.writelines(message)
+   print(message)
+   serialToArduino.write(endMark.encode())
+   print("sended message:",   message, endMark)
+   time.sleep(.5)
+   
 
 server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
@@ -23,11 +47,11 @@ server_socket.listen(1)
 client_socket,address = server_socket.accept()
 print("Accepted connection from ",address)
 
+print("Ready to communication")
+
 sender=threading.Thread(target=send,args=(client_socket,))
 receiver=threading.Thread(target=receive,args=(client_socket,))
 
 sender.start()
 receiver.start()
-while True:
- time.sleep(1)
- pass
+
