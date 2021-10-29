8c503dfc4aeff3e683896794f1009602706e7cbe
simjaeseo <delpho@naver.com>
2021-10-29 08:24:12 +0000

    [MODIFIED] app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/auth", require("./routes/auth.router"));
 app.use("/cosmo", authMiddleware, require("./routes/rent.router"));
 app.use("/cosmo1", authMiddleware, require("./routes/report.router"));
-app.use("/cosmo", require("./routes/return.router"));
+app.use("/cosmo2", require("./routes/return.router"));
 
 //여기는 이제 헬멧박스 open, 후방감지기능, 반납 기능 포함하는 라우터 1개(or 3개로 나눠서) 만들어야함(꼭 rentAuthMiddleware 거치게 만들기. )
 //app.use("/boxOpen", authMiddleware, rentAuthMiddleware, require("./routes/boxOpen.router"));
